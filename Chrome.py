1b03b4f2349d607ec44781dfe943627cf44f9ca7
Duncan Sykes <sykesduncan32@gmail.com>
2018-01-31 16:38:18 +1100

    Updated in python3 without GUI

--- Chrome.py
+++ Chrome.py
@@ -10,38 +10,38 @@ while True:
 5. Get Rick Rolled ;)
 6. Info about chromecast
     """
-    print menu
+    print(menu)
 
-    choose = raw_input(">")
+    choose = input(">")
 
     if choose == "1":
-	print "[*] Scanning..."
+        print("[*] Scanning...")
         chromecasts = pychromecast.get_chromecasts()
         myDevices = []
-	for device in chromecasts:
-  	    device.wait()
-    	    print(str(device))
+        for device in chromecasts:
+            device.wait()
+            print(str(device))
     elif choose == "2":
-        ip = raw_input("IP of Chrome Cast> ")
+        ip = input("IP of Chrome Cast> ")
         os.system('curl -H "Content-Type: application/json" http://%s:8008/apps/YouTube -X DELETE' % (ip))
 
     elif choose == "3":
-        ip = raw_input("IP of Chrome Cast> ")
+        ip =  input("IP of Chrome Cast> ")
         os.system('curl -H "Content-Type: application/json" http://%s:8008/setup/reboot -d "{"params":"now"}" -X POST' % (ip))
 
     elif choose == "4":
-        ip = raw_input("IP of Chrome Cast> ")
-	print "eg. v=dQw4w9WgXcQ"
-        video = raw_input("Link> ")
+        ip = input("IP of Chrome Cast> ")
+        print ("eg. v=dQw4w9WgXcQ")
+        video = ("Link> ")
         os.system('curl -H "Content-Type: application/json" http://%s:8008/apps/YouTube -X POST -d "%s"' % (ip, video))
         
     elif choose == "5":
-        ip = raw_input("IP of Chrome Cast> ")
+        ip = input("IP of Chrome Cast> ")
         os.system('curl -H "Content-Type: application/json" http://%s:8008/apps/YouTube -X POST -d "v=dQw4w9WgXcQ"' % (ip))
 
     elif choose == "6":
-        ip = raw_input("IP of Chrome Cast> ")
+        ip = input("IP of Chrome Cast> ")
         os.system('curl http://%s:8008/setup/eureka_info?options=detail | json_pp' % (ip))
 
     else:
-        print "Error!"
+        print("Error!")
